954b1ca23794ba7a3f1e41338815109b3866fa49
elecV2 <kucoin@icloud.com>
2021-04-13 09:52:22 +0800

    softupdate.js better

--- script/JSFile/softupdate.js
+++ script/JSFile/softupdate.js
@@ -1,21 +1,23 @@
 // （先看完说明，再决定是否执行！操作不可恢复，谨慎使用。执行前，先根据自身需求修改下面 CONFIG 变量里的内容）
 // 该脚本用于获取 https://github.com/elecV2/elecV2P 库的最新文件，并进行本地替换。（软更新升级）
+// 使用前请确保当前 elecV2P 服务器可正常连接 raw.githubusercontent.com/或自定义 cdngit 站点
 // 更新后会自动重启，以应用新的版本（请确定已保存任务列表及做好了储存映射数据备份等工作）
 // 脚本会先尝试以 PM2 的方式重启，如果失败，将直接重启容器(Docker 模式下)或服务器(pm2 指令不可用的情况下)
 // 3.1.8 版本后 elecV2P 默认启动方式更改为 PM2，建议在此版本后使用
 // 
 // 该文件更新地址: https://raw.githubusercontent.com/elecV2/elecV2P/master/script/JSFile/softupdate.js
-// 最近更新时间: 2021-04-09 22:30:45
+// 最近更新时间: 2021-04-13 22:30:45
 
 let CONFIG = {
   store: 'softupdate_CONFIG',    // 将当前配置内容(CONFIG 值) 常量储存。留空: 表示使用下面的参数进行更新，否则将会读取 store 中的 softupdate_CONFIG 对应值进行更新。如果 softupdate_CONFIG 尚未设置(首次运行)，会先按下面参数执行，并储存当前 CONFIG 内容
-  forceupdate: false,      // false: 检测到有新的版本时才更新。 true: 不检测版本直接更新
-  restart: 'all',          // false: 只更新文件，不重启不应用。 其他值表示 pm2 重启线程名，比如 all/elecV2P/index（暂时不清楚就保持不动）
+  forceupdate: false,            // false: 检测到有新的版本时才更新。 true: 不检测版本直接更新
+  restart: 'elecV2P',            // false: 只更新文件，不重启不应用。 其他值表示 pm2 重启线程名，比如 all/elecV2P/index（暂时不清楚就保持不动）
   noupdate: [
     'script/Store',        // 设置一些不覆盖更新的文件夹（保留个人数据）。根据个人需求进行调整
     // 'script/JSFile',    // 如果不设置，也只会覆盖更新 elecV2P 自带的同名文件，对其他文件无影响
+    // 'script/Shell',
     'script/Lists',
-    'script/Shell',
+    'rootCA',
     'Docker',              // 当文件夹或名称中包含 Docker 时，跳过下载更新
     'Todo',                // 排除单个文件，使用文件名包含的关键字即可
     '^\\.',                // 也可以使用正则表示式。匹配方式为 new RegExp(str).test(fileurl)
@@ -50,7 +52,12 @@ async function checkUpdate(){
     console.log('开始获取最新版本号...')
     let res = await $axios(CONFIG.cdngit + '/elecV2/elecV2P/master/package.json')
     let newversion = res.data.version
-    console.log('当前版本:', __version, '最新版本:', newversion)
+    if (newversion) {
+      console.log('当前版本:', __version, '最新版本:', newversion)
+    } else {
+      console.log('获取最新版本号失败，可能是网络存在问题，无法连接', CONFIG.cdngit)
+      return false
+    }
     if (__version !== newversion) {
       console.log('检测到有新的版本:', newversion)
       return true
