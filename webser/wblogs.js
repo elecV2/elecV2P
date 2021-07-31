06af43f3dba40aaa06160f7fae90d3db5f41465f
elecV2 <kucoin@icloud.com>
2021-08-01 00:10:45 +0800

    feat: logs 删除按钮

--- webser/wblogs.js
+++ webser/wblogs.js
@@ -20,16 +20,17 @@ module.exports = app => {
     res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
     res.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
     if (sType(logs) === 'array') {
-      res.write(`<title>elecV2P LOGS - ${logs.length}</title><style>body{display: flex;flex-wrap: wrap;justify-content: space-between;}.item {height: 40px;border-radius: 10px;padding: 0 0 0 15px;margin: 4px 8px;background: #1890ff;color: white;font-size: 18px;font-family: 'Microsoft YaHei', -apple-system, Arial;}.item_a {padding: 8px 0;color: white;text-decoration: none;}.item_delete {display: inline-flex;justify-content: center;align-items: center;width: 15px;cursor: pointer;opacity: 0;padding: 8px 0;border-radius: 0 10px 10px 0;background-color: red;}.item_delete:hover{opacity: 1;}</style>`)
+      res.write(`<title>elecV2P LOGS - ${logs.length}</title><style>.logs{padding: 0;margin: 0;display: flex;flex-wrap: wrap;justify-content: space-between;}.logs_item {height: 40px;display: inline-flex;align-items: center;line-height: 40px;list-style: none;border-radius: 10px;padding: 0 0 0 15px;margin: 4px 8px;background: #1890ff;color: white;font-size: 18px;font-family: 'Microsoft YaHei', -apple-system, Arial;}.logs_a {color: white;text-decoration: none;}.logs_delete {width: 15px;text-align: center;cursor: pointer;opacity: 0;border-radius: 0 10px 10px 0;background-color: red;}.logs_delete:hover{opacity: 1;}</style>`)
       if (logs.length === 0) {
-        res.write('<div class="item">暂无 LOGS 日志</div>')
+        res.write('<div class="logs_item"><span>暂无 LOGS 日志</span><span class="logs_delete"></span></div>')
       } else {
+        res.write('<ul class="logs">')
         logs.forEach(log=>{
           let rflog = `${filename !== 'all' ? (filename + '/') : ''}${log}`
-          res.write(`<div class='item'><a class='item_a' href="/logs/${rflog}" target="_blank">${log}</a><span class='item_delete' onclick="logDel('${rflog}', this)">x</span></div>`)
+          res.write(`<li class='logs_item'><a class='logs_a' href="/logs/${rflog}" target="_blank">${log}</a><span class='logs_delete' data-name='${rflog}'>x</span></li>`)
         })
+        res.write(`</ul><script type='text/javascript'>document.querySelector(".logs").addEventListener("click",t=>{let n=t.target.dataset.name;n&&confirm("确定删除日志 "+n+"？（不可恢复）")&&fetch("/logs",{method:"delete",headers:{"Content-Type":"application/json"},body:JSON.stringify({name:n})}).then(e=>e.json()).then(e=>{0===e.rescode?t.target.parentElement.remove():alert(e.message||e)}).catch(e=>{alert(n+" 删除失败 "+e.message),console.log(e)})});</script>`)
       }
-      res.write(`<script type='text/javascript'>function logDel(n,o){confirm("确定删除日志 "+n+"？（不可恢复）")&&fetch("/logs",{method:"delete",headers:{"Content-Type":"application/json"},body:JSON.stringify({name:n})}).then(e=>e.json()).then(e=>{0===e.rescode?o.parentElement.remove():alert(e.message||e)}).catch(e=>{alert(n+" 删除失败 "+e.message),console.log(e)})}</script>`)
       res.end()
     } else {
       res.write(`<title>${filename} - elecV2P</title><style>.logs{background:#1890ff;border-radius:10px;color:#fff;font-family:consolas, monospace;font-size:18px;height:fit-content; overflow-wrap:break-word;padding:8px 12px;text-decoration:none; white-space:pre-wrap; word-break:break-word;}</style>`)
