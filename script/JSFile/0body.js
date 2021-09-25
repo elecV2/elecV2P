1522b74d3354ab8014f6080fcb96a25f8f4e7e50
elecV2 <kucoin@icloud.com>
2021-09-25 20:44:55 +0800

    perf: 示例脚本优化

--- script/JSFile/0body.js
+++ script/JSFile/0body.js
@@ -1,4 +1,52 @@
-// $request.method, $request.protocol, $request.url, $request.hostname, $request.port, $request.path, $request.headers, $request.body
-// $response.status, $response.statusCode, $response.headers, $response.body
+// $request.url, $request.headers, $request.body<string>, $request.bodyBytes<buffer>
+// $request.method, $request.protocol, $request.hostname, $request.port, $request.path
+// $response.status, $response.statusCode, $response.headers, $response.body<string>, $response.bodyBytes<buffer>
 
-$done({ response: { body: 'elecV2P body' }})
+// 直接返回结果
+$done({
+  response: {
+    status: 200,        // 状态码，比如 401/404/502 等。默认为 200，可省略
+    headers: {          // 替换返回数据的头部信息。可省略
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/plain; charset=utf-8',     // 如使用替换 headers, 此项务必填写
+      'X-Powered-By': 'elecV2P'
+    },
+    body: 'hello elecV2P'   // 返回的 body 可以是任意数据类型
+  }
+})
+
+// 以上基本等同于
+// @grant nodejs
+$done({
+  bodyBytes: Buffer.from('hello elecV2P')
+})
+
+// 或者直接全部省略（不推荐
+$done(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x65, 0x6c, 0x65, 0x63, 0x56, 0x32, 0x50]).buffer)
+
+// 注意: 如果该脚本是在网络请求前执行，以上 $done 结果并不相同
+// 第一个有 response 参数，第三个为非 object 结果，都会直接返回结果（不发送网络请求
+// 而第二个只有 bodyBytes/body 参数表示修改网络请求体(request body)，然后继续网络请求
+
+// 关于网络请求前可修改的相关参数如下:（每一项都可单独设置，不必同时使用
+$done({
+  protocol: 'https',           // 当此项不等于当前网络请求协议时，将直接返回使用此协议
+  path: '/efss/test',
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'User-Agent': 'Mozilla/5.0 (Linux; U; elecV2P; x64) ePhone Super Max Plus++ XII'
+  },
+  body: {
+    "hello": "elecV2P"
+  },
+  bodyBytes: Buffer.from(`{"hello": "elecV2P bodyBytes"}`),     // bodyBytes 优化级高于 body
+})
+
+// 如果要进行 url 跳转，可以使用 301/307 statusCode
+$done({
+  response: {
+    statusCode: 307,
+    header: { Location: 'https://github.com/elecV2/elecV2P' }
+  }
+})
--- script/JSFile/favend.js
+++ script/JSFile/favend.js
@@ -1,15 +1,21 @@
 // EFSS favend 之 backend runjs 示例脚本
 // 该脚本用于 elecV2P EFSS 界面 favend 相关部分
-// 更新时间: 2021-09-17 
+// 更新时间: 2021-09-24
 // 文件地址: https://raw.githubusercontent.com/elecV2/elecV2P/master/script/JSFile/favend.js
 
-// $request.headers, $request.body, $request.method, $request.hostname, $request.port, $request.path, $request.url
-// $response.headers, $response.body, $response.statusCode
+// favend 中可获取的网络请求参数:
+// $request.url, $request.headers, $request.body<object|string> (favend 暂不支持 $request.bodyBytes<buffer>)
+// $request.method, $request.protocol, $request.hostname, $request.port, $request.path
+
+// favend 中的 response 待生成，故无 $response 相关参数
 
 if (/json/.test($request.url)) {
   $done({
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      'X-Powered-By': 'elecV2P'
+    },
     body: {
       'hello': 'elecV2P favend',
       'note': '这是由 elecV2P favend 返回的 JSON 数据',
@@ -17,11 +23,37 @@ if (/json/.test($request.url)) {
       'request': $request
     }
   })
+} else if (/pic/.test($request.url)) {
+  const fs = require('fs')
+  const path = require('path')
+
+  fs.readFile(path.join(__efss, 'favend.png'), (err, data) => {
+    if (err) {
+      $done({
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'text/plain;charset=utf-8',
+          'X-Powered-By': 'elecV2P'
+        },
+        body: err.message
+      })
+    } else {
+      $done({
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'image/png',
+          'X-Powered-By': 'elecV2P'
+        },
+        body: data
+      })
+    }
+  })
 } else {
   $done({
     headers: {
-      "Content-Type": "text/html;charset=utf-8"
+      'Content-Type': 'text/html;charset=utf-8',
+      'X-Powered-By': 'elecV2P'
     },
-    body: `<p>这是一个通过 elecV2P favend 生成的动态网页</p><p>该网络请求的头部信息(headers): <pre>${ JSON.stringify($request.headers, null, 2) }</pre></p><div>理论上可以包含任何 html 内容</div><p>关于 favend 的说明可参考 efss/readme.md 或 <a href='https://github.com/elecV2/elecV2P-dei/blob/master/docs/08-logger&efss.md' target='_blank'>此说明文档</a></p><div><a href='?json'>点击此处</a> 查看此 backend 返回的 JSON 数据</div><br><p>该 JS 地址: <a href='https://raw.githubusercontent.com/elecV2/elecV2P/master/script/JSFile/favend.js' target='_blank'>https://raw.githubusercontent.com/elecV2/elecV2P/master/script/JSFile/favend.js</a></p>`
+    body: `<p>这是一个通过 elecV2P favend 生成的动态网页</p><p>该网络请求的头部信息(headers): <pre>${ JSON.stringify($request.headers, null, 2) }</pre></p><div>理论上可以包含任何 html 内容</div><p>关于 favend 的说明可参考 efss/readme.md 或 <a href='https://github.com/elecV2/elecV2P-dei/blob/master/docs/08-logger&efss.md' target='_blank'>此说明文档</a></p><div><a href='?json'>点击此处</a> 查看此 backend 返回的 JSON 数据</div><br><div><a href='?pic' target='_blank'>点击此处</a> 查看此 backend 返回的一张图片</div><br><p>该 JS 地址: <a href='https://raw.githubusercontent.com/elecV2/elecV2P/master/script/JSFile/favend.js' target='_blank'>https://raw.githubusercontent.com/elecV2/elecV2P/master/script/JSFile/favend.js</a></p>`
   })
 }
