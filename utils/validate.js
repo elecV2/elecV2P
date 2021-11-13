e210382dbb80cb4a84c03acf7ef10dd734b10b3d
elecV2 <kucoin@icloud.com>
2021-11-13 21:17:07 +0800

    fix: rewrite 订阅默认背景 perf: cookie len 8 to 10

--- utils/validate.js
+++ utils/validate.js
@@ -22,7 +22,7 @@ function isAuthReq(req, res) {
   }
   let headstr = `${ipAddress} ${req.method} ${req.originalUrl || '/'},`
   let cookies = cookie.parse(req.headers.cookie || '')
-  if (cookies.token && (CONFIG.wbrtoken + CONFIG.wbrtoken).indexOf(atob(cookies.token)) !== -1) {
+  if (cookies?.token?.length > 10 && (CONFIG.wbrtoken + CONFIG.wbrtoken).indexOf(atob(cookies.token)) !== -1) {
     clog.debug(headstr, 'authorized by cookie')
     return true
   }
@@ -49,7 +49,7 @@ function isAuthReq(req, res) {
       if (res) {
         let days = req.query?.cookie === 'long' ? 365 : 7
         clog.notify('set cookie for', ipAddress, 'Max-Age:', days, 'days')
-        res.setHeader('Set-Cookie', cookie.serialize('token', btoa((CONFIG.wbrtoken + CONFIG.wbrtoken ).substr(iRandom(CONFIG.wbrtoken.length), 8)), {
+        res.setHeader('Set-Cookie', cookie.serialize('token', btoa((CONFIG.wbrtoken + CONFIG.wbrtoken ).substr(iRandom(CONFIG.wbrtoken.length), 10)), {
           // httpOnly: true,
           maxAge: 60 * 60 * 24 * days // cookie 有效期
         }))
