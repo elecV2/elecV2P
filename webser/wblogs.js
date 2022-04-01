e1781dfad6db965c3f328235d079262547711d4e
elecV2 <kucoin@icloud.com>
2022-04-01 15:38:07 +0800

    fix: logs background bug

--- webser/wblogs.js
+++ webser/wblogs.js
@@ -20,14 +20,14 @@ module.exports = app => {
     }
     res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
     res.write('<head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">')
-    let mainbk = '', appbk = '', style = ''
+    let mainbk = '#2890EE', appbk = '', style = ''
     if (CONFIG.webUI?.theme?.simple?.enable) {
-      mainbk = CONFIG.webUI.theme.simple.mainbk || '#1890ff'
+      mainbk = CONFIG.webUI.theme.simple.mainbk || '#2890EE'
       appbk  = CONFIG.webUI.theme.simple.appbk  || ''
       style  = CONFIG.webUI.theme.simple.style  || ''
     }
     if (sType(logs) === 'array') {
-      res.write(`<title>elecV2P LOGS - ${logs.length}</title><style>body{margin: 0;padding: 0;}#app{min-height: 100vh;background: ${appbk}}.logs{padding: 0;margin: 0;display: flex;flex-wrap: wrap;justify-content: space-between;}.logs_item {height: 40px;display: inline-flex;align-items: center;line-height: 40px;list-style: none;border-radius: 10px;padding: 0 0 0 15px;margin: 4px 8px;background: ${ mainbk };color: white;font-size: 18px;font-family: 'Microsoft YaHei', -apple-system, Arial;}.logs_a {color: white;text-decoration: none;}.logs_delete {width: 1em;text-align: center;cursor: pointer;opacity: 0;border-radius: 0 10px 10px 0;background-color: red;}.logs_delete:hover{opacity: 1;}${style}</style></head><body><div id='app'>`)
+      res.write(`<title>elecV2P LOGS - ${logs.length}</title><style>:root {--main-bk: ${ mainbk };--main-fc: #FAFAFD;--delt-bk: #FF2828;}body{margin: 0;padding: 0;}#app{min-height: 100vh;background: ${appbk}}.logs{padding: 0;margin: 0;display: flex;flex-wrap: wrap;justify-content: space-between;}.logs_item {height: 40px;display: inline-flex;align-items: center;line-height: 40px;list-style: none;border-radius: 10px;padding: 0 0 0 15px;margin: 4px 8px;background: var(--main-bk);color: var(--main-fc);font-size: 18px;font-family: 'Microsoft YaHei', -apple-system, Arial;}.logs_a {color: var(--main-fc);text-decoration: none;}.logs_delete {width: 1em;text-align: center;cursor: pointer;opacity: 0;border-radius: 0 10px 10px 0;background-color: var(--delt-bk);}.logs_delete:hover{opacity: 1;}${style}</style></head><body><div id='app'>`)
       if (logs.length === 0) {
         res.write('<div class="logs_item"><span>暂无 LOGS 日志</span><span class="logs_delete"></span></div>')
       } else {
@@ -40,7 +40,7 @@ module.exports = app => {
       }
       res.end('</div></body>')
     } else {
-      res.write(`<title>${filename} - elecV2P</title><style>.logs{background:#1890ff;border-radius:10px;color:#fff;font-family:consolas, monospace;font-size:18px;height:fit-content; overflow-wrap:break-word;padding:8px 12px;text-decoration:none; white-space:pre-wrap; word-break:break-word;}</style>`)
+      res.write(`<title>${filename} - elecV2P</title><style>:root {--main-bk: ${ mainbk };--main-fc: #FAFAFD;}.logs{background:var(--main-bk);border-radius:10px;color:var(--main-fc);font-family:consolas, monospace;font-size:18px;height:fit-content; overflow-wrap:break-word;padding:8px 12px;text-decoration:none; white-space:pre-wrap; word-break:break-word;}</style></head><body>`)
       logs.on('open', ()=>{
         res.write(`<div class='logs'>`)
       })
@@ -48,7 +48,7 @@ module.exports = app => {
         res.write(escapeHtml(chunk.toString()))
       })
       logs.on('close', ()=>{
-        res.end('</div>')
+        res.end('</div></body>')
       })
       logs.on('error', (err)=>{
         res.end(err)
